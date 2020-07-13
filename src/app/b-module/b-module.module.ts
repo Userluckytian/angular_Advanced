efc9bb8e7d9af96e975b7ba9c626aaec6d3bf551
Userluckytian <tianlukang123@163.com>
2020-07-13 21:32:18 +0800

    compare:比较模块生成的不同，以及正确的组织方式

---
+++ src/app/b-module/b-module.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BModuleComponent } from './b-module/b-module.component';
+
+@NgModule({
+  declarations: [BModuleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BModuleModule { }
